b3f24b96166a72a21cca7063e9f2ac4c9986c12d
Thefenix2000 <m.wos.biznes@gmail.com>
2022-11-24 13:46:52 +0100

    fix fix

--- src/components/ProjectDetail/ProjectDetail.jsx
+++ src/components/ProjectDetail/ProjectDetail.jsx
@@ -1,53 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import React from "react";
+import { useParams } from "react-router-dom";
 import { projects } from "../../helpers/Projects";
 import "./ProjectDetail.scss";
 function ProjectDetail() {
-  const [loading, setLoading] = useState(true);
   const { projectID } = useParams();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (projectID > projects.length || projectID < 0) {
-      navigate("/404");
-      setLoading(true);
-    }
-    setLoading(false);
-  }, []);
   const project = projects[projectID];
   return (
     <>
-      {!loading ? (
-        <div className="projectDetail">
-          <div className="detailContent">
-            <h2>{project.name}</h2>
-            <img src={project.img} alt="Project Image" />
-            <div className="skills">
-              <div className="skillsTitle">Skills:</div>
-              {project.skills.map((skill) => {
-                return (
-                  <div key={`${project.name}-${skill}`} className="skill">
-                    {skill}
-                  </div>
-                );
-              })}
-            </div>
-            <div className="buttons">
-              {project.buttons.map((button) => {
-                return (
-                  <a
-                    key={`${project.name}-${button.name}`}
-                    target="_blank"
-                    className="hire button"
-                    href={button.url}
-                  >
-                    {button.name}
-                  </a>
-                );
-              })}
-            </div>
+      <div className="projectDetail">
+        <div className="detailContent">
+          <h2>{project.name}</h2>
+          <img src={project.img} alt={project.name} />
+          <div className="skills">
+            <div className="skillsTitle">Skills:</div>
+            {project.skills.map((skill) => {
+              return (
+                <div key={`${project.name}-${skill}`} className="skill">
+                  {skill}
+                </div>
+              );
+            })}
+          </div>
+          <div className="buttons">
+            {project.buttons.map((button) => {
+              return (
+                <a
+                  key={`${project.name}-${button.name}`}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hire button"
+                  href={button.url}
+                >
+                  {button.name}
+                </a>
+              );
+            })}
           </div>
         </div>
-      ) : null}
+      </div>
     </>
   );
 }
